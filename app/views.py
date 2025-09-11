9a9d461f965b74ffc8832071ec51e953992aae3a
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2025-09-11 15:33:45 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("I am an app, I like containers")
+    return HttpResponse("I am MlOps Engineer, I like Docker!")
